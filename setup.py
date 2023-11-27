70a6828e3db1191690bc532d9975746e2fb3fa0f
Oleksii Tsybrovskyi <tsybrovsky@gmail.com>
2023-11-27 22:37:45 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="clean-folder",
+    version="1.1",
+    description="Clean You folder",
+    author="Alex_Tsybr",
+    license="MIT",
+    packages=["clean_folder"],
+)
